f69124fdb67175cc11a54104cb25a6675e8dea58
JulianaYS <u20201b782@upc.edu.pe>
2023-10-15 21:43:48 -0500

    feat(learning-services): added CategoryYSResponse and TutorialYSResponse

---
+++ LearningCenter.API/Learning/Domain/Services/Communication/CategoryYSResponse.cs
@@ -0,0 +1,15 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Shared.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Domain.Services.Communication;
+
+public class CategoryYSResponse : BaseResponse<CategoryYS>
+{
+    public CategoryYSResponse(string message) : base(message)
+    {
+    }
+
+    public CategoryYSResponse(CategoryYS resource) : base(resource)
+    {
+    }
+}
---
+++ LearningCenter.API/Learning/Domain/Services/Communication/TutorialYSResponse.cs
@@ -0,0 +1,15 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Shared.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Domain.Services.Communication;
+
+public class TutorialYSResponse: BaseResponse<TutorialYS>
+{
+    public TutorialYSResponse(string message) : base(message)
+    {
+    }
+
+    public TutorialYSResponse(TutorialYS resource) : base(resource)
+    {
+    }
+}
