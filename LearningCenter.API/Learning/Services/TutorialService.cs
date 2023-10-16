f33d82d0139936fd249545aa2a19ca5a64cc247f
JulianaYS <u20201b782@upc.edu.pe>
2023-10-16 18:43:51 -0500

    feat(learning-services): added TutorialService.

---
+++ LearningCenter.API/Learning/Services/TutorialService.cs
@@ -0,0 +1,45 @@
+﻿using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Repositories;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Domain.Services.Communication;
+
+namespace LearningCenter.API.Learning.Services;
+
+public class TutorialService : ITutorialYSService
+{
+    private readonly ITutorialYSRepository _tutorialYsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ICategoryYSRepository _categoryYsRepository;
+
+    public TutorialService(ITutorialYSRepository tutorialYsRepository, IUnitOfWork unitOfWork, ICategoryYSRepository categoryYsRepository)
+    {
+        _tutorialYsRepository = tutorialYsRepository;
+        _unitOfWork = unitOfWork;
+        _categoryYsRepository = categoryYsRepository;
+    }
+
+    public Task<IEnumerable<TutorialYS>> ListAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<TutorialYS>> ListByCategoryIdAsync(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialYSResponse> SaveAsync(TutorialYS tutorialYs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialYSResponse> UpdateAsync(int tutorialId, TutorialYS tutorialYs)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TutorialYSResponse> DeleteAsync(int tutorialId)
+    {
+        throw new NotImplementedException();
+    }
+}
