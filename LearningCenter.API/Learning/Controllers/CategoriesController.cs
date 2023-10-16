0a55f79e1df482003387377083538751c0dcc763
JulianaYS <u20201b782@upc.edu.pe>
2023-10-16 18:38:59 -0500

    feat(learning-controllers): added put, post and delete categories controller.

--- LearningCenter.API/Learning/Controllers/CategoriesController.cs
+++ LearningCenter.API/Learning/Controllers/CategoriesController.cs
@@ -2,6 +2,7 @@
 using LearningCenter.API.Learning.Domain.Models;
 using LearningCenter.API.Learning.Domain.Services;
 using LearningCenter.API.Learning.Resources;
+using LearningCenter.API.Shared.Extensions;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LearningCenter.API.Learning.Controllers;
@@ -25,5 +26,53 @@ public class CategoriesController : ControllerBase
         var resources = _mapper.Map<IEnumerable<CategoryYS>, IEnumerable<CategoryYSResource>>(categories);
 
         return resources;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveCategoryYSResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessage());
+
+        var category = _mapper.Map<SaveCategoryYSResource, CategoryYS>(resource);
+
+        var result = await _categoryYsService.SaveAsync(category);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var categoryResource = _mapper.Map<CategoryYS, CategoryYSResource>(result.Resource);
+
+        return Ok(categoryResource);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(int id, [FromBody] SaveCategoryYSResource resource)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState.GetErrorMessage());
+        
+        var category = _mapper.Map<SaveCategoryYSResource, CategoryYS>(resource);
+        var result = await _categoryYsService.UpdateAsync(id, category);
+        
+        if (!result.Success)
+            return BadRequest(result.Message);
+
+        var categoryResource = _mapper.Map<CategoryYS, CategoryYSResource>(result.Resource);
+
+        return Ok(categoryResource);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var result = await _categoryYsService.DeleteAsync(id);
+
+        if (!result.Success)
+            return BadRequest(result.Message);
+        
+        var categoryResource = _mapper.Map<CategoryYS, CategoryYSResource>(result.Resource);
+
+        return Ok(categoryResource);
     }
 }
