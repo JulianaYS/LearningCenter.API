a9e1867127a1fd540b0e38b4e0cbb77b0023711f
JulianaYS <u20201b782@upc.edu.pe>
2023-10-18 03:03:56 -0500

    feat(learning-resources): added DataAnnotations in SaveTutorialYSResource.

--- LearningCenter.API/Learning/Resources/SaveTutorialYSResource.cs
+++ LearningCenter.API/Learning/Resources/SaveTutorialYSResource.cs
@@ -1,8 +1,16 @@
-﻿namespace LearningCenter.API.Learning.Resources;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearningCenter.API.Learning.Resources;
 
 public class SaveTutorialYSResource
 {
+    [Required]
+    [MaxLength(50)]
     public string Title { get; set; }
+    
+    [MaxLength(120)]
     public string Description { get; set; }
+    
+    [Required]
     public int CategoryId { get; set; }
 }
