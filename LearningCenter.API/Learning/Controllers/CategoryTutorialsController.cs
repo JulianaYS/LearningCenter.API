f64e317ce66b40db05d0665fb06cf565b22935af
JulianaYS <u20201b782@upc.edu.pe>
2023-10-18 02:57:53 -0500

    feat(learning-controller): created and implemented CategoryTutorialsController.

---
+++ LearningCenter.API/Learning/Controllers/CategoryTutorialsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using LearningCenter.API.Learning.Domain.Models;
+using LearningCenter.API.Learning.Domain.Services;
+using LearningCenter.API.Learning.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningCenter.API.Learning.Controllers;
+
+[ApiController]
+[Route("/api/v1/categories/{categoryId}/tutorials")]
+public class CategoryTutorialsController : ControllerBase
+{
+    private readonly ITutorialYSService _tutorialService;
+    private readonly IMapper _mapper;
+    
+    public CategoryTutorialsController(ITutorialYSService tutorialService, IMapper mapper)
+    {
+        _tutorialService = tutorialService;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<TutorialYSResource>> GetAllByCategoryIdAsync(int categoryId)
+    {
+        var tutorials = await _tutorialService.ListByCategoryIdAsync(categoryId);
+
+        var resources = _mapper.Map<IEnumerable<TutorialYS>, IEnumerable<TutorialYSResource>>(tutorials);
+
+        return resources;
+    }
+
+}
