0812219d426443506cf413680dd28a962dd401f9
JulianaYS <u20201b782@upc.edu.pe>
2023-10-26 20:02:26 -0500

    feat(security-exceptions): added AppException for get info of error.

---
+++ LearningCenter.API/Security/Domain/AppException.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace LearningCenter.API.Security.Domain;
+
+public class AppException : Exception
+{
+    public AppException() :base() 
+    {
+    }
+
+    public AppException(string message) : base(message)
+    {
+    }
+
+    public AppException(string message, params object[] args) 
+        : base(string.Format(CultureInfo.CurrentCulture, message, args))
+    {
+    }
+}
